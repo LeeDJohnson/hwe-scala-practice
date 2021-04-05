2852c2452731a08fe1d00e76e7727e5d69cbd148
Lee Johnson <Lee.D.Johnson@gmail.com>
2021-04-04 19:27:26 -0500

    Week 2 Labwork

--- src/main/scala/com/farrellw/hwe/exercises/WordCount.scala
+++ src/main/scala/com/farrellw/hwe/exercises/WordCount.scala
@@ -4,12 +4,19 @@ object WordCount {
   /*
     Given a single sentence, return a list of words
    */
-  def splitSentenceIntoWords(sentence: String): List[String] = ???
+  def splitSentenceIntoWords(sentence: String): List[String] = {
+    sentence.split(" ").toList
+
+  }
 
   /*
     Given a single sentence, return a Map of words to their counts
    */
-  def sentenceWordCount(sentence: String): Map[String, Int] = ???
+  def sentenceWordCount(sentence: String): Map[String, Int] = {
+    val words = splitSentenceIntoWords(sentence)
+    val string = words.groupBy(x => x)
+    string.map(word => (word._1, word._2.length))
+  }
 
   /*
     Given a list of sentences, return a map of words to their counts
--- src/main/scala/com/farrellw/hwe/practice/OptionEither.scala
+++ src/main/scala/com/farrellw/hwe/practice/OptionEither.scala
@@ -9,18 +9,41 @@ object OptionEither {
     Returns age of a dog when given a human age.
     Returns None if the input is None.
   */
-  def dogAge(humanAge: Option[Int]): Option[Int] = ???
+  def dogAge(humanAge: Option[Int]): Option[Int] = {
+    humanAge.map(x => {
+      x * 7
+    })
+  }
 
   /*
     Returns the total cost af any item.
     If that item has a price, then the price + 7% of the price should be returned.
   */
-  def totalCost(item: Item): Option[Double] = ???
+  def totalCost(item: Item): Option[Double] = {
+    if (item.price.isDefined) {
+      Some(item.price.get * 1.07)
+    } else {
+      None
+    }
+  }
+
 
   /*
     Given a list of weather temperatures, calculates the average temperature across all weather stations.
     Some weather stations don't report temperature
     Returns None if the list is empty or no weather stations contain any temperature reading.
    */
-  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = ???
+  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = {
+    val withData = temperatures.filter(data => data.temperature.isDefined)
+    if (withData.isEmpty) {
+      None
+    } else {
+      val numberOfStations = withData.length
+      val sum = withData.map(temp => {
+        temp.temperature.get
+      }).sum
+      val average = sum / numberOfStations
+      Some(average)
+    }
+  }
 }
